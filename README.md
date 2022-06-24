df04fce2e52851ada8d6f8a6a03ac61ca8eb548f
Willem Zen <willem_zen@example.com>
2022-06-24 12:06:55 +0200

    Implement P6, P9

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # parallel_primes_again
 
-Compare Zig and Rust, see also https://github.com/zrayx/parallel_primes.
+Compare Zig and Rust, see also my Rust implementation: https://github.com/zrayx/parallel_primes
+
+At the moment I wouldn't be able to decide which to chose for a larger hobby project.
 
 Zig pros:
 * Legibility
   * The source code of the standard library is very readable
 * Much easier to implement code
+* More fun to code in, not as much fighting the language
+* Embedded programming actually viable
 
 Zig cons:
 * Documentation
@@ -14,3 +18,8 @@ Zig cons:
 * Tool support
   * Couldn't get zls/debugging to work out of the box in vim or vs code.
 * A hair slower than rust for very similar code
+* Language not yet stable
+* shadowing of variables not allowed: no local variable max if there is a global function called max
+* no closures
+* slow compile
+* no for loop
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const croc = std.testing.allocator;
+const dbg = std.debug.print;
 
 const Primes = struct {
     primes: std.ArrayList(u64),
@@ -7,7 +8,9 @@ const Primes = struct {
     const Self = @This();
 
     pub fn init() !Primes {
-        var primes = Primes{ .primes = std.ArrayList(u64).init(croc) };
+        var primes = Primes{
+            .primes = std.ArrayList(u64).init(croc),
+        };
         try primes.primes.append(2);
         try primes.primes.append(3);
         try primes.primes.append(5);
@@ -17,11 +20,20 @@ const Primes = struct {
         return primes;
     }
 
+    fn len(self: Self) u64 {
+        return self.primes.items.len;
+    }
+
+    fn last(self: Self) u64 {
+        const l = self.primes.items.len;
+        return self.primes.items[l - 1];
+    }
+
     fn deinit(self: Self) void {
         self.primes.deinit();
     }
 
-    fn is_prime1(n: u64) u64 {
+    fn isPrime(n: u64) u64 {
         if (n < 3) return 1;
         var i: u64 = 2;
         while (i * i <= n) : (i += 1) {
@@ -30,7 +42,7 @@ const Primes = struct {
         return 1;
     }
 
-    fn to_prime(self: *Self, n: u64) !u64 {
+    fn toPrime(self: *Self, n: u64) !u64 {
         if (n <= 1)
             return 0;
         if (n > 1 and n <= 3) {
@@ -56,9 +68,9 @@ const Primes = struct {
             return;
         }
 
-        var i: usize = 0;
-        while (true) : (i += 1) {
-            const p = self.primes.items[i];
+        for (self.primes.items) |p| {
+            // dbg("n: {d}, i: {d}\n", .{ n, i });
+            //dbg("c1: testing {d}%{d} at {d}\n", .{ n, p, i });
             if (p * p > n) break;
             if (n % p == 0) return;
         }
@@ -74,10 +86,177 @@ const Primes = struct {
         return list;
     }
 
+    // list must be empty
+    fn prime_slice_thread(self: *Self, start: u64, end: u64, list: *std.ArrayList(u64)) !void {
+        var i = start;
+        // dbg("b1 start: {d}, end: {d}, self.primes.len: {d}\n", .{ start, end, self.primes.items.len });
+        while (i < end) : (i += 2) {
+            try self.is_prime_store(i, list);
+        }
+        return;
+    }
+
     fn prime_slice_store(self: *Self, v: []const u64) !void {
         try self.primes.appendSlice(v);
     }
 };
+
+var timer: i64 = undefined;
+fn time_diff() i64 {
+    return std.time.milliTimestamp() - timer;
+}
+
+fn p10_thread(primes: *Primes, start: u64, end: u64, list: *std.ArrayList(u64)) !void {
+    return primes.prime_slice_thread(start, end, list);
+}
+
+// memoization in 1 thread
+fn p10(max_prime: u64) !void {
+    timer = std.time.milliTimestamp();
+
+    var sum: u64 = 6;
+    var primes = try Primes.init();
+    defer primes.deinit();
+
+    const STEP_SIZE: u64 = 6000000;
+    const MAX_THREADS: u64 = 32;
+    const THREADS: u64 = 32;
+
+    var last: u64 = 15; // primes up to 13 are already stored
+    var list: [MAX_THREADS]std.ArrayList(u64) = undefined;
+    var threads: [MAX_THREADS]std.Thread = undefined;
+
+    var i: usize = 0;
+    while (i < MAX_THREADS) : (i += 1) {
+        list[i] = std.ArrayList(u64).init(croc);
+    }
+
+    while (last < max_prime) {
+        const step_to_max = max((max_prime - last) / THREADS, 50);
+        const step_root = (last * last - last) / THREADS;
+        const step = max(min(min(step_to_max, step_root), STEP_SIZE), 40) & 0xfffffffffffffffe;
+        i = 0;
+        while (i < THREADS) : (i += 1) {
+            try list[i].resize(0);
+            threads[i] = try std.Thread.spawn(.{}, p9_thread, .{ &primes, last, last + step, &list[i] });
+            last = min(last + step, max_prime + 2 - step);
+        }
+
+        i = 0;
+        while (i < THREADS) : (i += 1) {
+            threads[i].join();
+            sum += list[i].items.len;
+        }
+
+        i = 0;
+        while (i < THREADS) : (i += 1) {
+            try primes.prime_slice_store(list[i].items);
+        }
+
+        last = primes.last();
+    }
+
+    std.debug.print("P10: Time elapsed: {}, sum: {}, max_prime: {}\n", .{ time_diff(), sum, max_prime });
+
+    i = 0;
+    for (list) |j| {
+        j.deinit();
+    }
+}
+
+fn p9_thread(primes: *Primes, start: u64, end: u64, list: *std.ArrayList(u64)) !void {
+    return primes.prime_slice_thread(start, end, list);
+}
+
+// memoization in 1 thread
+fn p9(max_prime: u64) !void {
+    timer = std.time.milliTimestamp();
+
+    var sum: u64 = 6;
+    var primes = try Primes.init();
+    defer primes.deinit();
+
+    const STEP_SIZE: u64 = 6000000;
+    const MAX_THREADS: u64 = 32;
+    const THREADS: u64 = 32;
+
+    var last: u64 = 15; // primes up to 13 are already stored
+    var list: [MAX_THREADS]std.ArrayList(u64) = undefined;
+    var threads: [MAX_THREADS]std.Thread = undefined;
+
+    var i: usize = 0;
+    while (i < MAX_THREADS) : (i += 1) {
+        list[i] = std.ArrayList(u64).init(croc);
+    }
+
+    while (last < max_prime) {
+        const step_to_max = max((max_prime - last) / THREADS, 50);
+        const step_root = (last * last - last) / THREADS;
+        const step = max(min(min(step_to_max, step_root), STEP_SIZE), 40) & 0xfffffffffffffffe;
+        i = 0;
+        while (i < THREADS) : (i += 1) {
+            try list[i].resize(0);
+            threads[i] = try std.Thread.spawn(.{}, p9_thread, .{ &primes, last, last + step, &list[i] });
+            last = min(last + step, max_prime + 2 - step);
+        }
+
+        i = 0;
+        while (i < THREADS) : (i += 1) {
+            threads[i].join();
+            sum += list[i].items.len;
+        }
+
+        i = 0;
+        while (i < THREADS) : (i += 1) {
+            try primes.prime_slice_store(list[i].items);
+        }
+
+        last = primes.last();
+    }
+
+    std.debug.print("P9: Time elapsed: {}, sum: {}, max_prime: {}\n", .{ time_diff(), sum, max_prime });
+
+    i = 0;
+    for (list) |j| {
+        j.deinit();
+    }
+}
+
+fn p6_thread(primes: *Primes, start: u64, end: u64, list: *std.ArrayList(u64)) !void {
+    return primes.prime_slice_thread(start, end, list);
+}
+
+// memoization in 1 thread
+fn p6(max_prime: u64) !void {
+    var time_start = std.time.milliTimestamp();
+
+    var sum: u64 = 6;
+    var primes = try Primes.init();
+    defer primes.deinit();
+
+    const STEP_SIZE: u64 = 3000;
+    const THREADS: u64 = 4;
+    var last: u64 = 15; // primes up to 13 are already stored
+    var list = std.ArrayList(u64).init(croc);
+    defer list.deinit();
+
+    while (last < max_prime) {
+        try list.resize(0);
+        const step_to_max = (max_prime - last) / THREADS;
+        const step_root = (last * last - last) / THREADS;
+        const step = max(min(min(step_to_max, step_root), STEP_SIZE), 4) & 0xfffffffffffffffe;
+
+        const thread = try std.Thread.spawn(.{}, p6_thread, .{ &primes, last, last + step, &list });
+        thread.join();
+
+        last += step;
+        sum += list.items.len;
+        try primes.prime_slice_store(list.items);
+    }
+
+    var time_elapsed = std.time.milliTimestamp() - time_start;
+    std.debug.print("P6: Time elapsed: {}, sum: {}, max_prime: {}\n", .{ time_elapsed, sum, max_prime });
+}
 
 fn p5(max_prime: u64) !void {
     var time_start = std.time.milliTimestamp();
@@ -122,7 +301,7 @@ fn p3(max_prime: u64) !void {
     defer primes.deinit();
 
     while (i < max_prime) : (i += 1) {
-        sum += try primes.to_prime(i);
+        sum += try primes.toPrime(i);
     }
 
     var time_elapsed = std.time.milliTimestamp() - time_start;
@@ -135,7 +314,7 @@ fn p1(max_prime: u64) void {
     var sum: u64 = 0;
     var i: u64 = 1;
     while (i < max_prime) : (i += 1) {
-        sum += Primes.is_prime1(i);
+        sum += Primes.isPrime(i);
     }
 
     var time_elapsed = std.time.milliTimestamp() - time_start;
@@ -143,9 +322,13 @@ fn p1(max_prime: u64) void {
 }
 
 pub fn main() anyerror!void {
-    try p5(3_000_000);
-    try p3(3_000_000);
-    p1(3_000_000);
+    const num = 3_000_000;
+    try p10(num);
+    try p9(num);
+    try p6(num);
+    try p5(num);
+    try p3(num);
+    p1(num);
 }
 
 test "basic test" {
